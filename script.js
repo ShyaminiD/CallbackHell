5cd53a2ff501ab1c818c16f2204f57ca1f593376
ShyaminiD <85381247+ShyaminiD@users.noreply.github.com>
2021-09-15 17:34:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+
+setTimeout( () => {
+    document.getElementById("print").innerHTML = "10"
+      setTimeout( () => {
+          document.getElementById("print").innerHTML = "9"
+            setTimeout( () => {
+              document.getElementById("print").innerHTML = "8"
+                setTimeout( () => { 
+                  document.getElementById("print").innerHTML = "7"
+                    setTimeout( () => {
+                     document.getElementById("print").innerHTML = "6"
+                        setTimeout( () => {
+                          document.getElementById("print").innerHTML = "5"
+                            setTimeout( () => {
+                              document.getElementById("print").innerHTML = "4"
+                                setTimeout( () => {
+                                  document.getElementById("print").innerHTML = "3"
+                                    setTimeout( () => {
+                                      document.getElementById("print").innerHTML = "2"
+                                        setTimeout( () => {
+                                          document.getElementById("print").innerHTML = "1"
+                                            setTimeout( () => {
+                                              document.getElementById("print").innerHTML = "HAPPY INDEPENDENCE DAY!!!"
+                                          }, 3000)
+                                      }, 3000)
+                                  }, 3000)
+                               }, 3000)
+                           }, 3000)
+                      }, 3000) 
+                 }, 3000)
+              }, 3000)
+          },3000)
+      }, 3000)
+  }, 3000)
+  
+  document.body.style.backgroundImage = "linear-gradient(to top, #51c546, #94cb57, #c1d272, #e2d995, #f6e4bc, #f8e3be, #fae1c1, #fbe0c4, #f8cda2, #f5bb80, #f0a860, #eb943f)";
+  document.body.style.backgroundRepeat ="no-repeat";
+  document.body.style.height = "100vh";
+  document.body.style.textAlign ="center";
+  
+ 
+
+ 
